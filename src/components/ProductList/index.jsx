c8e4567859cd43b7abb331b80c317724ac6ae7e0
Enrique Mejia <enriquemejiacano@gmail.com>
2021-05-25 15:10:17 -0500

    Updating favicon

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -5,7 +5,7 @@ const ProductList = ({products}) => {
   return (
     <section className="mx-auto bg-yellow-100 dark:bg-gray-600 overflow-hidden max-w-7xl pb-12 px-4 lg:px-8">
       <h1 className="section-title mt-12">Productos</h1>
-      <article className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {products.map((e, index) => (
           <Product key={index} name={e.name} image={e.image} description={e.description} />
         ))}
--- src/elements/Product/index.stories.js
+++ src/elements/Product/index.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Product from './index';
+import Miel from '../../assets/images/miel.jpg';
+
 
 export default {
   title: 'Element/Product',
@@ -11,6 +13,6 @@ const Template = (args) => <Product {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   name: 'Miel',
-  image: '/images/miel.jpg',
+  image: Miel,
   description: 'Explicabo veniam nesciunt ex sint cum eum voluptatem, placeat necessitatibus vitae exercitationem quas rem dicta optio.'
 };
